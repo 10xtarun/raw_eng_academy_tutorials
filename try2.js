6faa1e3a8d5f0227ae5efda245dbf2f2e5a6d73e
10xtarun <10xtarun@gmail.com>
2023-06-13 14:59:22 +0530

    todos project shifted to root folder

--- try2.js
+++
@@ -1,9 +0,0 @@
-const obj1 = {
-    name: "John",
-    age: 24,
-    greetings: function(){
-        console.log(" My details are: ", this.name, this.age)
-    }
-}
-
-obj1.greetings()
--- try_mongodb/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    MONGO_URI: "mongodb+srv://10xtarun:tarun123@cluster0.f3zruyr.mongodb.net/?retryWrites=true&w=majority"
-}
